c06f41cb51eb6c022c7c7464c4f1a431eb6e4bae
NatureGuy767 <persanantoine231@gmail.com>
2024-12-03 22:54:44 -0400

    I added the derived classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
+class Items
+{
+
+	
+
+private: 
+	std::string Id,title;
+	enum Type{SelfHelp,Fiction, Non_Fiction,Science,SCI_Fi,CD,DVD};
+
+	Type itemType;
+public:
+
+	 Items(std:: string ID, std::string title, Type itemType )
+	{
+		 this->Id = ID;
+		 this->title = title;
+		 this->itemType = itemType;
+	}
+};
+
+
+class CD : public Items
+{
+
+
+};
+
+
+class DVD : public Items
+{
+
+};
 
 int main()
 {
