787e217569e25ddbf0247d75b573d8c041015578
NatureGuy767 <persanantoine231@gmail.com>
2024-12-06 15:12:00 -0400

    Derive classes

--- main.cpp
+++ main.cpp
@@ -1,41 +1,101 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-class Items
-{
+using namespace std;
 
-	
+class Items {
+protected:
+   
+    int id;
+    string title;
+  
+    bool isAvailable;
 
-private: 
-	std::string Id,title;
-	enum Type{SelfHelp,Fiction, Non_Fiction,Science,SCI_Fi,CD,DVD};
+public:
+    enum ITEM_TYPE { CD, DVD };
+    ITEM_TYPE itemType;
+    enum CONTENT_TYPE { SelfHelp, Fiction, Non_Fiction, Science, SCI_Fi, Music};
+    CONTENT_TYPE contentType;
+  
+    Items() : id(0), title(""), itemType(CD), contentType(SelfHelp) ,isAvailable(true) {}
+    Items(int id, string title, ITEM_TYPE itemType, CONTENT_TYPE contentType)
+        : id(id), title(title), itemType(itemType), contentType(contentType) , isAvailable(true) {}
 
-	Type itemType;
+
+    virtual void setItemName(string itemName) = 0;
+    virtual void displayDetails() const = 0;
+
+    int getId() const { return id; }
+    string getTitle() const { return title; }
+    ITEM_TYPE getType() const { return itemType; }
+    bool getAvailability() const { return isAvailable; }
+
+    void setAvailability(bool status) { isAvailable = status; }
+
+
+    virtual ~Items() {}
+};
+
+
+// I have to add the books class
+class CD : public Items {
 public:
 
-	 Items(std:: string ID, std::string title, Type itemType )
-	{
-		 this->Id = ID;
-		 this->title = title;
-		 this->itemType = itemType;
-	}
+    CD() : Items(0, " ", Items::CD, Items::SelfHelp) {}
+    CD(int id, string title,  ITEM_TYPE itemType, CONTENT_TYPE contentType )
+        : Items(id,"",  itemType, contentType) {}
+
+
+    void setItemName(string itemName) override {
+        title = itemName;
+    }
+
+    void displayDetails() const override {
+        cout << "CD -> ID: " << id << ", Title: " << title << ", ITEM_TYPE: CD" << endl;
+    }
 };
 
+class DVD : public Items {
+public:
+  
+    DVD() : Items(0, "", Items::DVD, Items:: SelfHelp) {}
+    DVD(int id, string title, ITEM_TYPE itemType, CONTENT_TYPE contentType)
+        : Items(id, title, itemType,contentType) {}
 
-class CD : public Items
-{
+   
+    void setItemName(string itemName) override {
+        title = itemName;
+    }
 
-
+    void displayDetails() const override {
+        cout << "DVD -> ID: " << id << ", Title: " << title << ", ITEM_TYPE: DVD" << endl;
+    }
 };
 
+int main() {
 
-class DVD : public Items
-{
+    CD cd1(122, "Classical Music", Items::ITEM_TYPE::CD, Items::CONTENT_TYPE :: Music);
+    cd1.displayDetails();
 
-};
+ 
+    DVD dvd1(456, "Science Documentary", Items::ITEM_TYPE::DVD, Items::CONTENT_TYPE::Science );
+    dvd1.displayDetails();
 
-int main()
-{
+  
+    ifstream accountFiles("accounts.txt");
 
-	std::cout << "We're going to create a library System yay!!!!";
+    if (!accountFiles.is_open()) {
+        cout << "This file could not be opened" << endl;
+        return 1;
+    }
 
+    string readFile;
+    while (getline(accountFiles, readFile)) {
+        cout << readFile << endl;
+    }
+
+    accountFiles.close();
+
+    return 0;
 }
