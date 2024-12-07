a8cf866903987ff9510c7ab98c901b817cd8aee3
Bernisha Moreau <bernisham0@gmail.com>
2024-12-07 17:11:43 -0400

    new

--- operations.cpp
+++ operations.cpp
@@ -7,66 +7,70 @@
 
 using namespace std;
 
-class Operations 
+class Operations
 {
-	private : string searchItem;
-	private : int checkInCount = 0, checkOutCount = 0;
-	vector <string> vec;
 
-	public : 
+public:
+vector <string> vec;
+string searchItem;
+ int checkInCount = 0, checkOutCount = 0;
+	  
+
+
 	//set and get methods for the search item or the removed item
 	void setSearchItem(string i)
 	{
 		searchItem = i;
 	}
 
-	string getSearchItem() const
+	string getSearchItem() 
 	{
 		return searchItem;
 	}
 
-	void searchItem(string search) const
-{
-	ifstream myFile("items.txt");
-
-	string line;
-
-	int lineNumber = 0;
-
-	bool found = false;
-
-	if (!myFile.is_open())
+	void searchItem(string search) 
 	{
-		cout << "File is not opened";
-	}
+		ifstream myFile("items.txt");
+
+		string line;
+
+		int lineNumber = 0;
+
+		bool found = false;
+
+		if (!myFile.is_open())
+		{
+			cout << "File is not opened";
+		}
 
 		while (getline(myFile, line))
 		{
-				lineNumber++;
-				if (line.find(searchItem))
-				{
-					cout << "Found \"" << searchItem << "\" at line " << lineNumber << line ;
-				}
-				else if (!found)
-				{
-					cout << "\"" << search << "\" not found in the file" ;
-				}
-				else
-				{
-					cout << "Unable to open file.";
-				}
+			lineNumber++;
+			if (line.find(searchItem))
+			{
+				cout << "Found " << searchItem << " at line " << lineNumber << line;
+			}
+			else if (!found)
+			{
+				cout << search << " not found in the file";
+			}
+			else
+			{
+				cout << "Unable to open file.";
+			}
+			myFile.close();
 		}
-		myFile.close();
-	
-} //end of function
 
-	//Adds an item to inventory
-	void addItem(string id, string title, string type, bool availability = true) 
-	{	
+
+	} //end of function
+
+		//Adds an item to inventory
+	void addItem(string id, string title, string type, bool availability = true)
+	{
 		ofstream myFile("items.txt");
 		vec.push_back(id);
-		myFile << "ID : \ " << id  << "Type : \ " << type << "Title : \ " << title << "Available : \ " << availability;
-		
+		myFile << "ID : " << id << "Type : " << type << "Title : " << title << "Available : " << availability;
+
 	}
 
 	//removes item from inventory
@@ -81,7 +85,7 @@ class Operations
 		while (getline(myFile, line))
 		{
 			string id(line.begin(), line.begin() + line.find(" "));
-			cout <<"Deleted line : \n" << line;
+			cout << "Deleted line : /n" << line;
 		}
 	}
 
@@ -89,7 +93,7 @@ class Operations
 	void checkIn(string firstName, string lastName, string accountID, string type, string itemID, string title)
 	{
 		ofstream myFile("checkIn.txt");
-		myFile << firstName << lastName << accountID << itemID << type << title ;
+		myFile << firstName << lastName << accountID << itemID << type << title;
 		checkInCount++;
 	}
 	//checks out an item ie. when someone goes with an item
@@ -107,14 +111,14 @@ class Operations
 		int count = 0;
 		int position = 0;
 
-		for ( int i = 0; i < vec.size(); ++i) 
+		for (int i = 0; i < vec.size(); ++i)
 		{
 			while (getline(myFile, line))
 			{
-			
+
 				if (line.find(vec[i]))
 				{
-						count++;
+					count++;
 				}
 				else if (!line.find(vec[i]))
 				{
@@ -122,7 +126,7 @@ class Operations
 				}
 				else
 				{
-				cout << "Unable to open file.";
+					cout << "Unable to open file.";
 				}
 			}
 		}
@@ -131,4 +135,5 @@ class Operations
 		cout << "Popularity : " << count;
 	}
 };
+	
 
